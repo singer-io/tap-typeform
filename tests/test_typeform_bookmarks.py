3a5a5109cae79ede0435f5d2523135878ca82fba
pbegle <peter.begle@bytecode.io>
2021-06-14 13:10:25 -0700

    Fixing error with missing bookmark_test method.

--- tests/test_typeform_bookmarks.py
+++ tests/test_typeform_bookmarks.py
@@ -70,7 +70,6 @@ class TypeformBookmarks(TypeformBaseTest):
 
     def test_run(self):
         expected_streams =  self.expected_streams()
-        self.bookmarks_test(expected_streams)
 
         expected_replication_keys = self.expected_replication_keys()
         expected_replication_methods = self.expected_replication_method()
