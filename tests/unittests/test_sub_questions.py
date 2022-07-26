15141fef88a2f4ae5ce3af4c9fbad0cc465a0c91
Jay Tilala <jay.tilala@CDSYS.LOCAL>
2022-07-26 18:34:29 +0530

    updated unittest

--- tests/unittests/test_sub_questions.py
+++ tests/unittests/test_sub_questions.py
@@ -1,41 +1,44 @@
+import unittest
+from unittest import mock
 from tap_typeform.streams import fetch_sub_questions
+from parameterized import parameterized
 
-def test_fetched_sub_question():
-    """To verify that we are getting expected response or not for question group"""
-    
-    test_cases = [
-        {
-            'case1a': 'value1a', 'case1b': 'value1b', 'case1c': 'value1c',
-            'properties': {
-                'description': 'Group question',
-                'case1': 'value1','case2':'value2',
-                'fields': [
-                    {
-                        'id': 'id1', 'title': 'title1', 'ref': 'ref1'
-                    },
-                    {
-                        'id': 'id2','title': 'title2', 'ref': 'title2'
-                    }
-                ]
+class TestSubQuestionTest(unittest.TestCase):
+    @parameterized.expand([
+       ["Question group have sub-questions",
+            {
+                'case1a': 'value1a', 'case1b': 'value1b', 'case1c': 'value1c',
+                'properties': {
+                    'description': 'Group question',
+                    'case1': 'value1','case2':'value2',
+                    'fields': [
+                        {
+                            'id': 'id1', 'title': 'title1', 'ref': 'ref1'
+                        },
+                        {
+                            'id': 'id2','title': 'title2', 'ref': 'title2'
+                        }
+                    ]
+                },
+                'type': 'group'
             },
-            'type': 'group'},
-        {
-            'case2a': 'value2a', 'case2b': 'value2b', 'case2c': 'value2c',
-            'properties': {
-                'description': 'Group question', 'case3':'value3', 'case4':'value4'
+           [{'question_id': 'id1', 'title': 'title1', 'ref': 'ref1'}, {'question_id': 'id2', 'title': 'title2', 'ref': 'title2'}]
+        ],
+       ["Question group don't have sub-questions",
+           {
+                'case2a': 'value2a', 'case2b': 'value2b', 'case2c': 'value2c',
+                'properties': {
+                    'description': 'Group question', 'case3':'value3', 'case4':'value4'
+                },
+                'type': 'group'
             },
-            'type': 'group'
-        }]
-    expected_case = [
-        {
-            'question_id': 'id1', 'title': 'title1', 'ref': 'ref1'
-        },
-        {
-            'question_id': 'id2', 'title': 'title2', 'ref': 'title2'
-        }]
+           []
+       ]
+    ])
+    def test_fetched_sub_question(self, test_name, test_value, expected_value):
+        """
+        To verify that we are getting expected response or not for question group
+        """
 
-    for test_case in test_cases :
-        if test_case ['properties'].get('fields'):
-            assert expected_case == fetch_sub_questions(test_case)
-        else:
-            assert [] == fetch_sub_questions(test_case)
+        self.assertEqual(expected_value,  fetch_sub_questions(test_value))
+
