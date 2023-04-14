511adaa9bbf147de7d9a7b79f15816811ad84fac
kethan1122 <kcherukuri@talend.com>
2023-04-14 14:33:46 +0530

    changelog and verison bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-typeform",
-    version="2.0.1",
+    version="2.1.0",
     description="Singer.io tap for extracting data from the TypeForm Responses API",
     author="bytcode.io",
     url="http://singer.io",
