d3050f3b809db8bab4d666258c519b6de79f788e
Bryant Gray <bryant.gray@qlik.com>
2024-10-11 13:55:23 -0400

    pin required dependencies (#82)
    * pin required dependencies
    
    Co-authored-by: Kristiana Gerxhi <98350726+kgerxhi@users.noreply.github.com>
    
    * bump to v2.4.1, update changelog
    
    ---------
    
    Co-authored-by: Kristiana Gerxhi <98350726+kgerxhi@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -4,24 +4,24 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-typeform",
-    version="2.4.0",
+    version="2.4.1",
     description="Singer.io tap for extracting data from the TypeForm Responses API",
     author="bytcode.io",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     install_requires=[
         "singer-python==6.0.0",
-        "pendulum",
-        "ratelimit",
-        "backoff",
-        "requests",
-        "parameterized",
+        "pendulum==3.0.0",
+        "ratelimit==2.2.1",
+        "backoff==2.2.1",
+        "requests==2.32.3",
     ],
     extras_require={
         'dev': [
             'pylint',
             'ipdb',
             'nose2',
+            "parameterized",
         ]
     },
     entry_points="""
