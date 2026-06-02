6c810e71124afefbef9cd94f9dd559137b229523
Leslie VanDeMark <lvandemark@talend.com>
2026-06-02 20:19:30 +0000

    Bump requests to 2.33.0 and version to 2.4.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-typeform",
-    version="2.4.3",
+    version="2.4.4",
     description="Singer.io tap for extracting data from the TypeForm Responses API",
     author="bytcode.io",
     url="http://singer.io",
@@ -13,7 +13,7 @@ setup(
         "singer-python==6.0.1",
         "pendulum==3.0.0",
         "backoff==2.2.1",
-        "requests==2.32.4",
+        "requests==2.33.0",
     ],
     extras_require={
         'dev': [
